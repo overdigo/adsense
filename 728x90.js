9b2c8a774f9f97845d4e3270e58ec4ae8ce42eba
Rodrigo <overdigo@gmail.com>
2019-05-29 14:56:09 -0300

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,31 @@
+var increaserev = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+if(increaserev>=728)
+    {
+document.write('<div class="InMargin_Top" style="position:relative; text-align: center; margin: 5px 5px 5px 5px;">');
+google_ad_client = "ca-pub-5232270347895348";
+google_ad_slot = "7535940055";
+google_ad_width = 728; 
+google_ad_height = 90;
+document.write('<script type="text/javascript" async src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>');
+document.write('</div>');
+} 
+else if(increaserev>=350)
+ {
+document.write('<div class="InMargin_Top" style="position:relative; text-align: center; margin: 5px 5px 5px 5px;">');
+google_ad_client = "ca-pub-5232270347895348";
+google_ad_slot = "7535940055";
+google_ad_width = 336; 
+google_ad_height = 280;
+document.write('<script type="text/javascript" async src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>');
+document.write('</div>');
+} 
+else
+ {
+document.write('<div class="InMargin_Top" style="position:relative; text-align: center; margin: 5px 5px 5px 5px;">');
+google_ad_client = "ca-pub-5232270347895348";
+google_ad_slot = "7535940055";
+google_ad_width = 300; 
+google_ad_height = 250;
+document.write('<script type="text/javascript" async src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>');
+document.write('</div>');
+}
