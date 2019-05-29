afde80e32d40000c94eca2ee183ade7d1bae174d
Rodrigo <overdigo@gmail.com>
2019-05-29 15:34:32 -0300

    Update and rename 336x280.js to ad336x280.js

--- ad336x280.js
+++ ad336x280.js
@@ -1,11 +1,11 @@
 var increaserev = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-if(increaserev>=420)
-{
+if(increaserev>=420) {
 document.write('<div class="InMargin_Top" style="position:relative; text-align: center; margin: 5px 2px 5px 2px;">');
 google_ad_client = "ca-pub-5232270347895348";
 google_ad_slot = "1531085830";
 google_ad_width = 336; 
 google_ad_height = 280;
+document.write('<script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>');
 document.write('</div>');
 } else {
 document.write('<div class="InMargin_Top" style="position:relative; text-align: center; margin: 5px 2px 5px 2px;">');
@@ -13,5 +13,6 @@ google_ad_client = "ca-pub-5232270347895348";
 google_ad_slot = "1531085830";
 google_ad_width = 300; 
 google_ad_height = 250;
+document.write('<script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>');
 document.write('</div>');
 }
